4cf19de4240134ad6bff694c8b08257a40474ffb
阳九 <871921282@qq.com>
2023-08-22 11:58:50 +0800

    将注册到collection抽离出来

--- src/IOC/serviceCollection.ts
+++ src/IOC/serviceCollection.ts
@@ -2,7 +2,7 @@
  * @Author: Luzy
  * @Date: 2023-08-18 15:24:37
  * @LastEditors: Luzy
- * @LastEditTime: 2023-08-20 15:42:14
+ * @LastEditTime: 2023-08-22 11:39:30
  * @Description: 用于保存Service实例或描述器的集合
  */
 
@@ -30,15 +30,7 @@ export class ServiceCollection {
         return this._entries.get(id);
     }
 
-
-
-    // 注册为单例  传入id和构造函数 创建描述器 后续会使用描述器进行实例化
-    registerSingleton<T>(id: ServiceIdentifier<T>, ctor: new (...args: any[]) => T): void {
-        const descriptor = new SyncDescriptor(ctor, [])
-        this.set(id, descriptor);
-    }
 }
-
 
 // 类描述器  保存了构造函数和参数  用于实例化
 export class SyncDescriptor<T> {
@@ -52,6 +44,19 @@ export class SyncDescriptor<T> {
     }
 }
 
+
+// 
+const globalServiceCollection = new ServiceCollection()
+
+// 注册为单例  传入id和构造函数 创建描述器 后续会使用描述器进行实例化
+export function registerSingleton<T>(id: ServiceIdentifier<T>, ctor: new (...args: any[]) => T): void {
+    const descriptor = new SyncDescriptor(ctor, [])
+    globalServiceCollection.set(id, descriptor);
+}
+
+export function getGlobalCollection(): ServiceCollection {
+    return globalServiceCollection
+}
 
 
 
