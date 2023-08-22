3d8f41e8217ea4f0cd25be57d632f9ad0db34315
阳九 <871921282@qq.com>
2023-08-22 15:50:22 +0800

    抽离forge配置

--- build/forge.config.js
+++ build/forge.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Luzy
+ * @Date: 2023-08-21 21:21:50
+ * @LastEditors: Luzy
+ * @LastEditTime: 2023-08-22 15:48:43
+ * @Description: electron-forge构建配置
+ */
 module.exports = {
   packagerConfig: {
     "asar": true
