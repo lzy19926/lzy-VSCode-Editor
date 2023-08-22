04684fd9fb6a7d0a2aeb3a45f2590ee4a3d451a8
阳九 <871921282@qq.com>
2023-08-22 18:44:11 +0800

    sidebar加载文件按钮功能测试

--- src/workbench/parts/SideBar.ts
+++ src/workbench/parts/SideBar.ts
@@ -2,18 +2,30 @@
  * @Author: Luzy
  * @Date: 2023-08-22 11:36:46
  * @LastEditors: Luzy
- * @LastEditTime: 2023-08-22 12:57:01
- * @Description: 
+ * @LastEditTime: 2023-08-22 18:35:32
+ * @Description: 左侧文件资源管理器view模块
  */
 
 import { createDecorator } from '../../common/IOC/decorator'
 import { registerSingleton } from '../../common/IOC/serviceCollection'
+import { IEditorService } from './EditorPart'
 import { Part } from './Part'
 
 export class SideBarPart implements ISideBarService, Part {
-    create(container: HTMLElement): void {
+
+    private _container!: HTMLElement
+
+    constructor(
+        @IEditorService private readonly editorService: IEditorService
+    ) {
 
     }
+
+    create(container: HTMLElement): void {
+        this._container = container
+
+    }
+
 }
 
 export interface ISideBarService { }
--- src/workbench/parts/TitleBar.ts
+++ src/workbench/parts/TitleBar.ts
@@ -2,18 +2,60 @@
  * @Author: Luzy
  * @Date: 2023-08-22 11:36:46
  * @LastEditors: Luzy
- * @LastEditTime: 2023-08-22 12:57:17
- * @Description: 
+ * @LastEditTime: 2023-08-22 18:42:37
+ * @Description: 顶部导航菜单栏
  */
 
 import { createDecorator } from '../../common/IOC/decorator'
 import { registerSingleton } from '../../common/IOC/serviceCollection'
+import { IEditorService } from './EditorPart'
 import { Part } from './Part'
 
 
 export class TitleBarPart implements ITitleBarService, Part {
+    private _container!: HTMLElement
+
+    constructor(
+        @IEditorService private readonly editorService: IEditorService
+    ) {
+
+    }
+
     create(container: HTMLElement): void {
 
+        this._container = container
+
+        this.createButton()
+    }
+
+
+
+
+    createButton() {
+        const btn = document.createElement("input")
+        btn.innerText = "打开文件测试"
+        btn.type = "file"
+        btn.onchange = this.readFileTest.bind(this)
+        this._container.appendChild(btn)
+    }
+
+
+    // 加载文件测试
+    readFileTest(event: Event) {
+        /**@ts-ignore*/
+        const file = event.target?.files?.[0]
+        const editor = this.editorService
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                const text = reader.result;
+                if (typeof text == 'string') {
+                    editor.loadFileContent(text)
+                }
+            };
+            reader.readAsText(file);
+        }
     }
 }
 
