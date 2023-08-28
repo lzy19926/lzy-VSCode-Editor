2a4d23ca75259beb8166db96b9b0dc988a977f8f
阳九 <871921282@qq.com>
2023-08-28 12:26:08 +0800

    TerminalSevice增加ws开启端口检查

--- src/common/TerminalService.ts
+++ src/common/TerminalService.ts
@@ -2,13 +2,14 @@
  * @Author: Luzy
  * @Date: 2023-08-22 11:36:46
  * @LastEditors: Luzy
- * @LastEditTime: 2023-08-28 11:32:04
+ * @LastEditTime: 2023-08-28 12:25:33
  * @Description: 提供terminal相关服务
  * 1. 接收前端xterm输入信息,并转到node-pty中执行
  */
 
 import { createDecorator } from './IOC/decorator'
 import { registerSingleton } from './IOC/serviceCollection'
+import { checkPort } from './utils'
 import * as os from 'os'
 import * as pty from 'node-pty'
 import WebSocket from 'ws';
@@ -16,15 +17,48 @@ import WebSocket from 'ws';
 export class TerminalService {
 
     private term?: pty.IPty
+    private ws_port: number = 9000 // 终端链接用ws开启端口
 
     constructor() {
-        this.start()
+      
     }
 
     //todo 启动一个终端(可改进为支持多个终端  使用pid标记)
-    public start() {
-        this.createTerminal()
-        this.startWebsocketServer()
+    public async start(): Promise<number> {
+        const freePort = await this.getFreePort()
+
+        if (freePort == -1) {
+            console.warn("Start Terminal Failed")
+        } else {
+            this.createTerminal()
+            this.startWebsocketServer(freePort)
+            this.ws_port = freePort
+        }
+
+        return this.ws_port
+    }
+
+    // 顺延三次获取可用端口
+    async getFreePort(): Promise<number> {
+        let retry = 0
+        let port = this.ws_port
+
+        const fn = async () => {
+            const isPortFree = await checkPort(port)
+
+            if (retry <= 3 && !isPortFree) {
+                retry++
+                port++
+                await fn()
+            } else if (retry > 3) {
+                console.warn("Could not find an available port. Retried 3 times")
+                port = -1
+            }
+        }
+
+        await fn()
+
+        return port
     }
 
     // 创建一个新的伪终端进程并将其附加到当前会话中
@@ -42,12 +76,12 @@ export class TerminalService {
     }
 
     // 启动ws长连接-port:9999,与前端xtrem进行交互
-    startWebsocketServer() {
-        const wss = new WebSocket.Server({ port: 9999 });
+    startWebsocketServer(port: number) {
+        const wss = new WebSocket.Server({ port });
         const term = this.term as pty.IPty
 
         wss.on('connection', function connection(ws) {
-            console.log('[Server] A client Connected.');
+            console.log('[Terminal Server] A client Connected.');
 
             term.onData(data => {
                 ws.send(data.trim());
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,4 @@
-
+import * as net from 'net'
 
 /**
  * @description: // 解析query字符串参数为对象
@@ -20,4 +20,31 @@ export function parseUrlQuery(url: string) {
     }
 
     return obj;
+}
+
+
+/**
+ *检查某个端口是否可用（空闲）
+*/
+export function checkPort(port: number): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        try {
+            const server = net.createServer();
+
+            server.on('error', () => {
+                console.log(`The port ${port} is occupied.`);
+                resolve(false)
+            });
+
+            // 及时关闭可避免增加系统负担
+            server.listen(port, () => {
+                server.close();
+                resolve(true)
+            });
+
+        } catch (err) {
+            reject(err)
+        }
+    })
+
 }
